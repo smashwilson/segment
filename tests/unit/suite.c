278ff1f6488cab8f41df06797b30fc7171c3d1b8
Ash Wilson <smashwilson@gmail.com>
2014-04-11 17:46:19 -0400

    Return non-zero exit status for failures.

--- tests/unit/suite.c
+++ tests/unit/suite.c
@@ -18,7 +18,10 @@ int main(void)
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
   CU_basic_run_tests();
+
+  unsigned int failures = CU_get_number_of_tests_failed();
+
   CU_cleanup_registry();
 
-  return CU_get_error();
+  return failures > 0;
 }

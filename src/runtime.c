7548894554e01006d6d7b329a60b5eabf9959217
Ash Wilson <smashwilson@gmail.com>
2014-06-28 17:33:43 -0400

    Create a runtime containing a basic symboltable.

--- src/runtime.c
+++ src/runtime.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+
 #include "runtime.h"
+
+struct seg_runtime {
+  seg_symboltable *symboltable;
+};
+
+seg_runtime *seg_new_runtime()
+{
+  seg_runtime *r = malloc(sizeof(seg_runtime));
+
+  r->symboltable = seg_new_symboltable();
+
+  return r;
+}
+
+seg_symboltable *seg_runtime_symboltable(seg_runtime *runtime)
+{
+  return runtime->symboltable;
+}
+
+void seg_delete_runtime(seg_runtime *runtime)
+{
+  seg_delete_symboltable(runtime->symboltable);
+  free(runtime);
+}
--- tests/unit/runtime_tests.c
+++ tests/unit/runtime_tests.c
@@ -2,10 +2,17 @@
 
 #include "unit.h"
 #include "runtime.h"
+#include "symboltable.h"
 
 static void test_creation(void)
 {
-  CU_FAIL("pending");
+  seg_runtime *r = seg_new_runtime();
+  CU_ASSERT_PTR_NOT_NULL_FATAL(r);
+
+  seg_symboltable *symtable = seg_runtime_symboltable(r);
+  CU_ASSERT_PTR_NOT_NULL(symtable);
+
+  seg_delete_runtime(r);
 }
 
 CU_pSuite initialize_runtime_suite(void)

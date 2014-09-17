435559aa0cf3e34b8f5ddcc64f132456bc51c2e8
Ash Wilson <smashwilson@gmail.com>
2014-09-17 16:23:23 -0400

    Populate error messages in object.c.

--- src/model/object.c
+++ src/model/object.c
@@ -18,39 +18,39 @@ seg_object *seg_class(seg_object *object, seg_runtime *r)
 
 seg_err seg_integer(int64_t value, seg_object **out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_integer");
 }
 
 seg_err seg_integer_value(seg_object *object, int64_t *out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_integer_value");
 }
 
 // SEG_STRINGLIKE //////////////////////////////////////////////////////////////////////////////////
 
 seg_err seg_string(char *str, uint64_t length, seg_object **out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_string");
 }
 
 seg_err seg_cstring(const char *str, seg_object **out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_cstring");
 }
 
 seg_err seg_symbol(char *str, uint64_t length, seg_object **out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_symbol");
 }
 
 seg_err seg_string_name(seg_object *stringlike, char **out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_string_name");
 }
 
 seg_err seg_string_length(seg_object *stringlike, uint64_t *out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_string_length");
 }
 
 
@@ -58,42 +58,42 @@ seg_err seg_string_length(seg_object *stringlike, uint64_t *out)
 
 seg_err seg_slotted(seg_object *class, seg_object **out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_slotted");
 }
 
 seg_err seg_slotted_capacity(seg_object *instance, uint64_t *out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_slotted_capacity");
 }
 
 seg_err seg_slotted_length(seg_object *instance, uint64_t *out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_slotted_length");
 }
 
 seg_err seg_slotted_setlength(seg_object *instance, uint64_t length)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_slotted_setlength");
 }
 
 seg_err seg_slotted_grow(seg_object *instance, uint64_t capacity)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_slotted_grow");
 }
 
 seg_err seg_ivar_at(seg_object *o, uint64_t index, seg_object **out)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_ivar_at");
 }
 
 seg_err seg_ivar_atput(seg_object *o, uint64_t index, seg_object *v)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_ivar_atput");
 }
 
 // BOOTSTRAPPING ///////////////////////////////////////////////////////////////////////////////////
 
 seg_err _seg_bootstrap_runtime(seg_runtime *runtime, seg_bootstrap_objects *bootstrap)
 {
-  return SEG_NOTYET;
+  return SEG_NOTYET("seg_bootstrap_runtime");
 }
